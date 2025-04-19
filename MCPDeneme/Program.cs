ca1f10c2e51b44af35af7ce024faa46cedf855d6
kadirdundar <100041382+kadirdundar@users.noreply.github.com>
2025-04-19 11:51:28 +0300

    FirstCommit

---
+++ MCPDeneme/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(settings:null);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+}
